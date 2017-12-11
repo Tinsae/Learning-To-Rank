be511f6ac9fc30783d31989b29ed73b4c54d7872
Tinsae <tigial3535@gmail.com>
2017-12-12 01:24:05 +0300

    Added sample comment

--- LEXP/src/application/Main.java
+++ LEXP/src/application/Main.java
@@ -29,7 +29,7 @@ import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
-
+// Commented for testing github
 public class Main extends Application implements Initializable {
 	
 	@FXML
